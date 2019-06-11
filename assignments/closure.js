ece208ea0f645409c8b611f6849df5ebd1948529
maddiestrange <maddie.strange@gmail.com>
2019-06-11 16:36:44 -0500

    completed closure stretch challenges

--- assignments/closure.js
+++ assignments/closure.js
@@ -22,16 +22,35 @@ basket();
 
 
 // ==== Challenge 2: Create a counter function ====
+var count = 0;
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  count = count + 1;
+  console.log(count)
 };
+counter();
+counter();
+
+ 
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
+var newcount = 0;
+const counterFactory = (method) => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+  if(method === 'increment'){
+    newcount = newcount + 1;
+  }
+  if(method === 'decrement'){
+    newcount = newcount - 1;
+  }
+  console.log(newcount)
 };
+
+counterFactory('increment');
+counterFactory('increment');
+counterFactory('decrement');
